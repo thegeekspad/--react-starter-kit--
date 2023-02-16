b959a01da77dcdc5e6636d3fc30d77803d51f59b
thegeekspad <thegeekspad@gmail.com>
2023-02-16 07:06:40 +0000

    Add header, footer and unit test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div class="title">React Starter Kit</div>
     <App />
   </React.StrictMode>
 );
